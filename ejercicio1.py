a7c841e038044ef4244795884f767bad62647a65
ldaa99-examenfinal_metodos-nxzfajnp <ldaa99-examenfinal_metodos-nxzfajnp@local>
2018-05-25 00:59:03 +0000

    ejerc1

--- ejercicio1.py
+++ ejercicio1.py
@@ -9,8 +9,8 @@ x = np.linspace(0,2.,10)
 fx = np.array([0., 0.0494, 0.1975, 0.4444, 0.7901,1.2346 , 1.7778, 2.4198, 3.1605, 4.])
 
 h=2.0/10
-#segunda derivada, donde y1 es el siguiente valor, y es el valor actual y 1y es el valor anterior, las y recorreran la lista fx y el h se haya desde las diferencias equiespaciadas de x 
+#segunda derivada, donde y1 es el siguiente valor, y es el valor actual y yy es el valor anterior, las y recorreran la lista fx y el h se haya desde las diferencias equiespaciadas de x 
 
-def seg(y, y1, 1y):
-    s=(1/h**2)*(y1-2*y+1y)
+def seg(y, y1, yy):
+    s=(1/h**2)*(y1-2*y+yy)
     return s
